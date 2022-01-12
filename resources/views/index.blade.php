ede3e9fb8d0265719c2d3af5044320cb2c01b981
Scott Munday <scottmunday84@gmail.com>
2022-01-12 11:02:07 -0500

    Submit more metadata. Keywords are pointless.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,9 @@
 
         <title>sitesbymunday.com - IT Software Consultation</title>
 
+        <meta name="description" content="Personal IT software consultation at a reasonable price. Serving the Indianapolis area.">
+        <meta name="robots" content="index, follow">
+
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     </head>
